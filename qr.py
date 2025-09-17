86247fd60caf0713dd30ff646e779cb364538dca
ninaksrs <ninaksrs@gmail.com>
2025-09-17 15:59:30 +0700

    generate qr picture

--- qr.py
+++ qr.py
@@ -1,8 +1,8 @@
 import qrcode as qr
-import image as core
+import image
 qr_link = 'www.sanook.com'
 
 qr_pic = qr.make(qr_link)
 qr_path = "qr_link_"+qr_link+".png"
 
-qr_pic.save(qr_pic)
+qr_pic.save(qr_path)
